702c5174b8e8612a8833a42e203bf5298f901571
pixelstuermer <philipp.montesano@hs-furtwangen.de>
2017-09-05 15:53:19 +0200

    Added JUnit dependency

---
+++ src/test/java/password4me/VarietyTest.java
@@ -0,0 +1,14 @@
+package password4me;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VarietyTest {
+
+   @Test
+   public void test() {
+      fail( "Not yet implemented" );
+   }
+
+}
